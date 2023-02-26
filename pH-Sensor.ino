5cd3db4cda86dec4c0d8e17cfe4f9c8040e3542b
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-26 15:30:40 +0530

    pH-Sensor

---
+++ pH-Sensor.ino
@@ -0,0 +1,39 @@
+#define SensorPin A0
+#define Offset 0         
+unsigned long int avgValue;
+float b;
+int buf[10],temp;
+ 
+void setup()
+{
+  pinMode(SensorPin,INPUT);  
+  Serial.begin(9600);  
+}
+void loop()
+{
+  for(int i=0;i<10;i++) { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++) {
+    for(int j=i+1;j<10;j++) {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      
+    avgValue+=buf[i];
+  avgValue = avgValue/6;
+  float phValue=(float)((avgValue)*
+					5.0/1024); 
+  phValue=3.5*phValue+Offset;
+  Serial.print("pH: ");  
+  Serial.print(phValue,2);
+  Serial.println(" ");
+  delay(800);
+}
